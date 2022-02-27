5f1902dd83ac71d7b49e14e3475de7b66932a58f
codehunter <nguyenthetoan1606@gmail.com>
2022-02-28 00:27:02 +0700

    Switch case and If else condition

---
+++ First/switch.js
@@ -0,0 +1,29 @@
+const day = 'monday';
+
+switch (day) {
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory video');
+        break;
+    case 'wednesday':
+        console.log('video code example');
+        break;
+    case 'thursday':
+        console.log('javascript fundermental');
+        break;
+    case 'friday':
+        console.log('python from zero to hero');
+        break;
+    case 'saturday':
+        console.log('do exercise');
+        break;
+    case 'sunday':
+        console.log('Enjoy the weeken :D');
+        break;
+    default:
+        console.log('working all day :D');
+        break;
+}
